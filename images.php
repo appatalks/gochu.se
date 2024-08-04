db696d665c6a1fb20082f42b9626d6cffac5de9a
Steven B. <4163156+appatalks@users.noreply.github.com>
2024-08-04 11:29:02 -0700

    Add files via upload

---
+++ images.php
@@ -0,0 +1,16 @@
+<?php
+echo ' 
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+
+<a href="images/gochuhot.jpg"><img src="images/gochuhot.jpg" width="280" height="125" title="Gochu Logo" alt="Gochu Logo Pepper" /></a>
+
+<a href="images/gochugirl.jpg"><img src="images/gochugirl.jpg" width="280" height="125" title="Gochu Girl" alt="Girl with Pepper" /></a>
+
+<a href="images/gochubowl.jpg"><img src="images/gochubowl.jpg" width="280" height="125" title="Gochu Bowl" alt="Bowl of Peppers" /></a>
+
+<a href="images/pepperbg.png"><img src="images/pepperbg.png" width="280" height="125" title="Pepper Background" alt="Chilli Pepper" /></a>
+
+</html>
+';
+?>
